3286e46d76f502a3f34b6131815ce8a200bedef9
c-coffee <4165426@qq.com>
2017-08-01 17:43:44 +0800

    2017.7.31 showUserInfo todo updateInfo

--- src/views/User.vue
+++ src/views/User.vue
@@ -5,26 +5,26 @@
     <div class="User">
         <el-dialog title="添加用户"  :visible.sync="dlg_isShowAddUserDlg" size="small" :close-on-click-modal="dlg_modelCloseOption">
             <el-form :model="addUserForm" :label-position="dlg_labelPosition" :rules="addUser_rules" ref="addUserForm" label-width="80px">
-                <el-form-item label="用户姓名" prop="name" class="is-required">
-                    <el-input  auto-complete="off" v-model="addUserForm.name"></el-input>
+                <el-form-item label="用户姓名" prop="username" class="is-required">
+                    <el-input v-model="addUserForm.username"></el-input>
                 </el-form-item>
-                <el-form-item label="登录帐号" prop="loginName" class="is-required">
-                    <el-input  auto-complete="off" v-model="addUserForm.loginName"></el-input>
+                <el-form-item label="登录帐号" prop="userid" class="is-required">
+                    <el-input  auto-complete="off" v-model="addUserForm.userid"></el-input>
                 </el-form-item>
-                <el-form-item label="初始密码" prop="pwd" class="is-required">
-                    <el-input  auto-complete="off" value="123" v-model="addUserForm.pwd"></el-input>
+                <el-form-item label="初始密码" prop="password" class="is-required">
+                    <el-input  auto-complete="off" type="password" value="123" v-model="addUserForm.password"></el-input>
                 </el-form-item>
-                <el-form-item label="联系手机" >
+                <el-form-item label="联系手机" prop="tel">
                     <el-input  auto-complete="off" v-model="addUserForm.tel"></el-input>
                 </el-form-item>
-                <el-form-item label="所属部门" prop="dept"  class="is-required">
-                    <el-select v-model="addUserForm.dept" placeholder="请选择部门...">
-                        <el-option label="业务室" value="4"></el-option>
-                        <el-option label="财务室" value="5"></el-option>
-                        <el-option label="综合科" value="6"></el-option>
-                        <el-option label="检验一室" value="1"></el-option>
-                        <el-option label="检验二室" value="2"></el-option>
-                        <el-option label="检验三室" value="3"></el-option>
+                <el-form-item label="所属部门" prop="department" class="is-required">
+                    <el-select v-model="addUserForm.department" placeholder="请选择部门...">
+                        <el-option
+                                v-for="item in deptInfo"
+                                :key="item.id"
+                                :label="item.deptName"
+                                :value="item.deptName">
+                        </el-option>
                     </el-select>
                 </el-form-item>
             </el-form>
@@ -36,25 +36,22 @@
         <el-dialog title="查看用户"  :visible.sync="dlg_isShowUserInfoDlg" size="small" :close-on-click-modal="dlg_modelCloseOption">
             <el-form :model="showUserInfoForm" :label-position="dlg_labelPosition" ref="showUserInfoForm" label-width="80px">
                 <el-form-item label="用户姓名">
-                    <el-input  auto-complete="off" readonly="readonly" v-model="showUserInfoForm.name"></el-input>
+                    <el-input  auto-complete="off" readonly="readonly" v-model="showUserInfoForm.username"></el-input>
                 </el-form-item>
                 <el-form-item label="登录帐号">
-                    <el-input  auto-complete="off" readonly="readonly" v-model="showUserInfoForm.loginName"></el-input>
-                </el-form-item>
-                <el-form-item label="初始密码">
-                    <el-input  auto-complete="off" readonly="readonly" value="123" v-model="showUserInfoForm.pwd"></el-input>
+                    <el-input  auto-complete="off" readonly="readonly" v-model="showUserInfoForm.userid"></el-input>
                 </el-form-item>
                 <el-form-item label="联系手机" >
                     <el-input  auto-complete="off" readonly="readonly" v-model="showUserInfoForm.tel"></el-input>
                 </el-form-item>
                 <el-form-item label="所属部门">
-                    <el-select v-model="showUserInfoForm.dept" placeholder="请选择部门...">
-                        <el-option label="业务室" value="4"></el-option>
-                        <el-option label="财务室" value="5"></el-option>
-                        <el-option label="综合科" value="6"></el-option>
-                        <el-option label="检验一室" value="1"></el-option>
-                        <el-option label="检验二室" value="2"></el-option>
-                        <el-option label="检验三室" value="3"></el-option>
+                    <el-select  v-model="showUserInfoForm.department" placeholder="请选择部门...">
+                        <el-option
+                                v-for="item in deptInfo"
+                                :key="item.id"
+                                :label="item.deptName"
+                                :value="item.id.toString()">
+                        </el-option>
                     </el-select>
                 </el-form-item>
                 <el-form-item label="用户签名">
@@ -67,26 +64,23 @@
         </el-dialog>
         <el-dialog title="修改用户"  :visible.sync="dlg_isShowUpdateUserDlg" size="small" :close-on-click-modal="dlg_modelCloseOption">
             <el-form :model="updateUserInfoForm" :label-position="dlg_labelPosition" :rules="addUser_rules" ref="updateUserForm" label-width="80px">
-                <el-form-item label="用户姓名" prop="name" class="is-required">
-                    <el-input  auto-complete="off" v-model="updateUserInfoForm.name"></el-input>
+                <el-form-item label="用户姓名" prop="username" class="is-required">
+                    <el-input  auto-complete="off" v-model="updateUserInfoForm.username"></el-input>
                 </el-form-item>
-                <el-form-item label="登录帐号" prop="loginName" class="is-required">
-                    <el-input  auto-complete="off" readonly="readonly" v-model="updateUserInfoForm.loginName"></el-input>
-                </el-form-item>
-                <el-form-item label="初始密码" prop="pwd" class="is-required">
-                    <el-input  auto-complete="off" value="123" v-model="updateUserInfoForm.pwd"></el-input>
+                <el-form-item label="登录帐号" prop="userid" class="is-required">
+                    <el-input  auto-complete="off" v-model="updateUserInfoForm.userid"></el-input>
                 </el-form-item>
                 <el-form-item label="联系手机" >
                     <el-input  auto-complete="off" v-model="updateUserInfoForm.tel"></el-input>
                 </el-form-item>
-                <el-form-item label="所属部门" prop="dept"  class="is-required">
-                    <el-select v-model="updateUserInfoForm.dept" placeholder="请选择部门...">
-                        <el-option label="业务室" value="4"></el-option>
-                        <el-option label="财务室" value="5"></el-option>
-                        <el-option label="综合科" value="6"></el-option>
-                        <el-option label="检验一室" value="1"></el-option>
-                        <el-option label="检验二室" value="2"></el-option>
-                        <el-option label="检验三室" value="3"></el-option>
+                <el-form-item label="所属部门" prop="department"  class="is-required">
+                    <el-select v-model="updateUserInfoForm.department" placeholder="请选择部门...">
+                        <el-option
+                                v-for="item in deptInfo"
+                                :key="item.id"
+                                :label="item.deptName"
+                                :value="item.id.toString()">
+                        </el-option>
                     </el-select>
                 </el-form-item>
             </el-form>
@@ -129,38 +123,38 @@
         <div class="userTable">
             <div class="pageNum">
                 <el-pagination
-                        :page-sizes="[10, 30, 100]"
-                        :page-size="10"
+                        :page-sizes="pageSizes"
+                        :page-size="pageSize"
                         layout="total, sizes, prev,  pager, next"
-                        :total="1000">
+                        :total="totalUser">
                 </el-pagination>
             </div>
             <el-table
-                    :data="userData"
+                    :data="userList"
                     border
                     style="width: 100%">
                 <el-table-column width="70" prop="id" label="序号"></el-table-column>
                 <el-table-column
-                        width="150"
-                        prop="name"
+                        width="120"
+                        prop="username"
                         label="姓名"
                         >
                 </el-table-column>
                 <el-table-column
-                        width="150"
-                        prop="userId"
+                        width="120"
+                        prop="userid"
                         label="用户名"
                         >
                 </el-table-column>
                 <el-table-column
                         width="150"
-                        prop="email"
+                        prop="tel"
                         label="手机"
                        >
                 </el-table-column>
                 <el-table-column
-                        width="180"
-                        prop="dept"
+                        width="120"
+                        prop="department"
                         label="所属部门"
                         >
                 </el-table-column>
@@ -169,8 +163,8 @@
                         label="操作"
                         >
                     <template scope="scope">
-                        <el-button type="text" size="small" @click="dlg_isShowUserInfoDlg=true;">查看</el-button>
-                        <el-button type="text" size="small" @click="dlg_isShowUpdateUserDlg=true;">修改</el-button>
+                        <el-button type="text" size="small" @click="showUserInfo(scope.$index)">查看</el-button>
+                        <el-button type="text" size="small" @click="showUpdateUserInfo(scope.$index);">修改</el-button>
                         <el-button type="text" size="small" @click="dlg_isUploadSignDlg=true;">上传签名</el-button>
                         <el-button type="text" size="small" @click="dlg_isDeleteUserDlg=true;">删除</el-button>
                     </template>
@@ -195,142 +189,95 @@ export default{
             deptInfo:[], //部门：下拉选项  created 时初始化
 
             //表格显示数据
-            page_size:10,
-            current_page:1,
+            pageSizes:[10,30,50],
+            pageSize:10,
+            currentPage:1,
+            totalUser:1000,
+
 
             dlg_modelCloseOption:false,
             dlg_labelPosition:'right',
             addUserForm:{
-                name:'',
-                loginName:'',
-                pwd:'',
+                username:'',
+                userid:'',
+                password:'',
                 tel:'',
-                dept:''
+                department:''
             },
-            showUserInfoForm:{
-                name:'张三',
-                loginName:'zhangsan',
-                pwd:'123',
-                tel:'13912345678',
-                dept:'3'
-            },
-            updateUserInfoForm:{
-                name:'张三',
-                loginName:'zhangsan',
-                pwd:'123',
-                tel:'13912345678',
-                dept:'3'
-            },
+            showUserInfoForm:{},
+            updateUserInfoForm:{},
             addUser_rules:{
-                name:[
+                username:[
                     {required:true,message:'请输入用户姓名',trigger:'blur'}
                 ],
-                loginName:[
+                userid:[
                     {required:true,message:'请输入登录用户名',trigger:'blur'}
                 ],
-                pwd:[
+                password:[
                     {required:true,message:'请输入初始密码',trigger:'blur'}
                 ],
-                dept:[
+                department:[
                     {required:true,message:'请选择所属部门',trigger:'change'}
                 ]
             },
-            userData:[
-                {
-                    id:1,
-                    name:'张三',
-                    userId:'zhangSan',
-                    email:'13912345678',
-                    dept:'检验一室'
-                },
-                {
-                    id:2,
-                    name:'张三',
-                    userId:'zhangSan',
-                    email:'13912345678',
-                    dept:'检验一室'
-                },
-                {
-                    id:3,
-                    name:'张三',
-                    userId:'zhangSan',
-                    email:'13912345678',
-                    dept:'检验一室'
-                },
-                {
-                    id:4,
-                    name:'张三',
-                    userId:'zhangSan',
-                    email:'13912345678',
-                    dept:'检验一室'
-                },
-                {
-                    id:5,
-                    name:'张三',
-                    userId:'zhangSan',
-                    email:'13912345678',
-                    dept:'检验一室'
-                },
-                {
-                    id:6,
-                    name:'张三',
-                    userId:'zhangSan',
-                    email:'13912345678',
-                    dept:'检验一室'
-                },
-                {
-                    id:7,
-                    name:'张三',
-                    userId:'zhangSan',
-                    email:'13912345678',
-                    dept:'检验一室'
-                },
-                {
-                    id:8,
-                    name:'张三',
-                    userId:'zhangSan',
-                    email:'13912345678',
-                    dept:'检验一室'
-                },
-                {
-                    id:9,
-                    name:'张三',
-                    userId:'zhangSan',
-                    email:'13912345678',
-                    dept:'检验一室'
-                },
-                {
-                    id:10,
-                    name:'张三',
-                    userId:'zhangSan',
-                    email:'13912345678',
-                    dept:'检验一室'
-                },
-                {
-                    id:11,
-                    name:'张三',
-                    userId:'zhangSan',
-                    email:'13912345678',
-                    dept:'检验一室'
-                },
-                {
-                    id:12,
-                    name:'张三',
-                    userId:'zhangSan',
-                    email:'13912345678',
-                    dept:'检验一室'
-                }
-            ]
+            userList:[]
         }
     },
     methods:{
-        getUser(){
+        showUserInfo(index){
+            this.$ajax.get('http://localhost:3333/user/getUserByUserId',
+                {
+                    params:{
+                        userid:this.userList[index].userid
+                    }
+                })
+                .then(function(res){
+                    this.showUserInfoForm = res.data[0];
+                    console.log(res);
+                }.bind(this))
+                .catch();
 
+            this.dlg_isShowUserInfoDlg = true;
+        },
+        showUpdateUserInfo(index){
+            this.$ajax.get('http://localhost:3333/user/getUserByUserId',
+                {
+                    params:{
+                        userid:this.userList[index].userid
+                    }
+                })
+                .then(function(res){
+                    this.updateUserInfoForm = res.data[0];
+                    console.log(this.updateUserInfoForm);
+                }.bind(this))
+                .catch();
+
+            this.dlg_isShowUpdateUserDlg = true;
         },
         addUser(){
             this.$refs['addUserForm'].validate(function(valid){
                 if(valid){
-                    this.dlg_isShowAddUserDlg = false;
+                    //添加用户信息,需要将密码进行md5加密
+                    this.$ajax.post('http://localhost:3333/user/addUser',this.addUserForm)
+                        .then(function(res){
+                            console.log('res',res);
+                            if(res.data.result===1){
+                                //录入成功，给出提示信息并关闭当前窗口
+                                this.$message({
+                                    showClose:true,
+                                    message:res.data.msg,
+                                    type:'success'
+                                });
+                                this.dlg_isShowAddUserDlg = false;
+                                //重置输入窗口
+                                this.$refs.addUserForm.resetFields();
+                            }else if(res.data.result===0){
+                                //该用户id已经存在，让用户id窗口获取到焦点 并给出提示
+
+                            }
+                        }.bind(this)).catch(function(err){
+                            console.log('err',err);
+                        }.bind(this));
                 }else{
                     console.log('error submit!');
                     return false;
@@ -340,6 +287,7 @@ export default{
         updateUser(){
             this.$refs['updateUserForm'].validate(function(valid){
                 if(valid){
+                    //todo 完成数据提交
                     this.dlg_isShowUpdateUserDlg = false;
                 }else{
                     console.log('error submit!');
@@ -348,16 +296,30 @@ export default{
             }.bind(this));
         }
     },
-    created:function(){
-        console.log('aaaa');
+    beforeMount:function(){
         //获取dept信息
         this.$ajax.get('http://localhost:3333/base/getDeptList')
             .then(function(res){
-                console.log(res);
-            })
+                this.deptInfo = res.data;
+                //需要bind(this);
+            }.bind(this))
             .catch(function(err){
                 console.log(err);
+            });
+        //获取用户列表信息
+        this.$ajax.get('http://localhost:3333/user/getUserList',
+            {
+                params:{
+                    pageSize:this.pageSize,
+                    currentPage:this.currentPage
+                }
+
             })
+            .then(function(res){
+                this.totalUser = res.data.total;
+                this.userList = res.data.users;
+            }.bind(this))
+            .catch();
     }
 }
 </script>
